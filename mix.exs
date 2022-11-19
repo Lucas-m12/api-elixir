798b078746e272542fe9a54d01570da4f9142d59
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2022-11-19 02:05:49 -0300

    feature: config deps

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule Api.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:argon2_elixir, "~> 3.0"}
     ]
   end
 
